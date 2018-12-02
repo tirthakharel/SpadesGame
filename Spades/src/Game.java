4693414b9244200134c8f5aa2a0a4be2dadb41f9
Tirtha Kharel <tkharel21@gmail.com>
2018-12-01 20:13:46 -0500

    Edited main Game file for JFrame window

--- Spades/src/Game.java
+++ Spades/src/Game.java
@@ -1,6 +1,13 @@
-public class Game {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-    public static void main(String args[]){
-        
+public class Game implements Runnable {
+    public void run(){
+
+        final JFrame frame = new JFrame("TOP LEVEL FRAME");
+        frame.setLocation(600, 450);
     }
+
+    public static void main(String[] args) { SwingUtilities.invokeLater(new Game()); }
 }
