5bb32cbee54b6913ed1d990ce661d3cb9c4183c8
Tirtha Kharel <tkharel21@gmail.com>
2018-12-01 22:27:45 -0500

    Trial commit for IntelliJ

--- Spades/src/Game.java
+++ Spades/src/Game.java
@@ -5,8 +5,13 @@ import javax.swing.*;
 public class Game implements Runnable {
     public void run(){
 
-        final JFrame frame = new JFrame("TOP LEVEL FRAME");
+        final JFrame frame = new JFrame("Spades");
         frame.setLocation(600, 450);
+
+        //Puts the frame on screen
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) { SwingUtilities.invokeLater(new Game()); }
